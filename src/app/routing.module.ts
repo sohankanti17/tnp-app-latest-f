f103ec27a6a4e85ceb8d733d0edc3f2f84eccbb1
sohankanti17 <sohan.kanti@bvicam.in>
2019-08-27 17:26:05 +0530

    2 files modified

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule} from '@angular/router'
 
-const routes: Routes = [
+import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
+import { PostsWrapperComponent } from './components/posts-wrapper/posts-wrapper.component';
 
+const routes: Routes = [
+  { path: "companies", component:  WelcomePageComponent},
+  { path: "companies/:slug", component: PostsWrapperComponent },
+  { path: "**", redirectTo: "/companies" }
 ]
 
 @NgModule({
@@ -14,4 +19,5 @@ const routes: Routes = [
   declarations: [],
   exports: [RouterModule]
 })
+
 export class RoutingModule { }
