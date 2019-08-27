628091a1f833ebeafb672b4d2fbbb08559e82059
sohankanti17 <sohan.kanti@bvicam.in>
2019-08-27 21:49:43 +0530

    4 files modified

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -6,12 +6,18 @@ import { WelcomePageComponent } from './components/welcome-page/welcome-page.com
 import { PostsWrapperComponent } from './components/posts-wrapper/posts-wrapper.component';
 import { CompanyInfoComponent } from './components/company-info/company-info.component';
 
+// const routes: Routes = [
+//   { path: "companies", outlet: "mid-content", component:  WelcomePageComponent},
+//   { path: "companies/:slug", children: [
+//     { path: "", outlet: "mid-content", component: PostsWrapperComponent},
+//     { path: "", outlet: "right-content", component: CompanyInfoComponent}
+//   ]},
+//   { path: "**", redirectTo: "/companies" }
+// ]
+
 const routes: Routes = [
-  { path: "companies", outlet: "mid-content", component:  WelcomePageComponent},
-  { path: "companies/:slug", children: [
-    { path: "", outlet: "mid-content", component: PostsWrapperComponent},
-    { path: "", outlet: "right-content", component: CompanyInfoComponent}
-  ]},
+  { path: "companies", component:  WelcomePageComponent},
+  { path: "companies/:slug", component: PostsWrapperComponent },
   { path: "**", redirectTo: "/companies" }
 ]
 
